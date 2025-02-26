57656ff08ef201d1922fcfc6c041ea9a370cb238
Gaetan Beguin <gaetan.beguin@gmail.com>
2025-02-26 09:53:38 +0000

    application finale ajout du bootstrap

--- app.R
+++ app.R
@@ -2,9 +2,14 @@ library(shiny)
 library(ggplot2)
 library(DT)
 library(glue)
+library(shinythemes)
+library(bslib)
+
+thematic_shiny(font="auto")
 
 # Interface utilisateur (UI)
 ui <- fluidPage(
+  theme = bs_theme(version = 5, bootswatch = "minty"),
   titlePanel("Exploration des Diamants"),
   
   sidebarLayout(
@@ -24,7 +29,10 @@ ui <- fluidPage(
       radioButtons(inputId = "color_rose", 
                    label = "Colorier les points en rose ?", 
                    choices = c("Oui" = TRUE, "Non" = FALSE), 
-                   selected = TRUE)
+                   selected = TRUE),
+      
+      actionButton(inputId = "show_notification", 
+                   label = "Afficher une notification")
     ),
     
     mainPanel(
@@ -37,20 +45,34 @@ ui <- fluidPage(
 # Serveur
 server <- function(input, output, session) {
   filtered_data <- reactive({
-    subset(diamonds, color == input$filter_color & price <= input$max_price)
+    subset(diamonds, 
+           color == input$filter_color & 
+             price <= input$max_price)
   })
   
+  # Affichage du nuage de points
   output$scatter_plot <- renderPlot({
-    ggplot(filtered_data(), aes(x = carat, y = price)) +
+    p <- ggplot(filtered_data(), aes(x = carat, y = price)) +
       geom_point(alpha = 0.6, color = ifelse(input$color_rose, "pink", "black")) +
       labs(title = glue("Prix max: {input$max_price} | Couleur: {input$filter_color}"),
            x = "Carat",
            y = "Prix") +
-      theme_minimal()
+      theme_minimal() +
+      theme(plot.background = element_rect(fill = "gray98"))
+    
+    p
   })
   
+  # Affichage du tableau interactif
   output$data_table <- renderDT({
-    datatable(filtered_data(), options = list(pageLength = 10, scrollX = TRUE))
+    datatable(filtered_data(), 
+              options = list(pageLength = 10, scrollX = TRUE))
+  })
+  
+  # Notification lors du clic sur le bouton
+  observeEvent(input$show_notification, {
+    showNotification(glue("Prix max: {input$max_price} | Couleur: {input$filter_color}"),
+                     type = "message")
   })
 }
 
